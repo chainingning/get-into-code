666b03582233324e371d486dd1682d8fda1b668b
chaining <=>
2021-06-22 10:13:17 +0800

    feat: 策略模式

--- design-patterns/src/main/java/com/chaining/patterns/behavioral/state/StatePatternDemo.java
+++ design-patterns/src/main/java/com/chaining/patterns/behavioral/state/StatePatternDemo.java
@@ -10,11 +10,8 @@ package com.chaining.patterns.behavioral.state;
 public class StatePatternDemo {
     public static void main(String[] args) {
         Context context = new Context();
-
         StartState startState = new StartState();
         startState.doAction(context);
         System.out.println(context.getState().toString());
-
-
     }
 }
