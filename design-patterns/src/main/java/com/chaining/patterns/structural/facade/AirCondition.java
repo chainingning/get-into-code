b203e1cc7312538b0296db5db4acbdd02de2a594
chaining <chaining>
2021-10-26 09:28:05 +0800

    feat: 外观模式

---
+++ design-patterns/src/main/java/com/chaining/patterns/structural/facade/AirCondition.java
@@ -0,0 +1,18 @@
+package com.chaining.patterns.structural.facade;
+
+/**
+ * <p>
+ * description
+ * <p>
+ *
+ * @author ning.chai@foxmail.com 2021/10/26 9:21
+ */
+public class AirCondition {
+    public void on(){
+        System.out.println("打开了空调");
+    }
+
+    public void off(){
+        System.out.println("关闭了空调");
+    }
+}
---
+++ design-patterns/src/main/java/com/chaining/patterns/structural/facade/Light.java
@@ -0,0 +1,18 @@
+package com.chaining.patterns.structural.facade;
+
+/**
+ * <p>
+ * description
+ * <p>
+ *
+ * @author ning.chai@foxmail.com 2021/10/26 9:21
+ */
+public class Light {
+    public void on(){
+        System.out.println("打开了灯");
+    }
+
+    public void off(){
+        System.out.println("关闭了灯");
+    }
+}
