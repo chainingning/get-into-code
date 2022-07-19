6e2123a8db1de6c41d3d832197556659a7e944f2
chaining <ning.chai@foxmail.com>
2022-07-19 17:16:08 +0800

    feat: 注解式开发启动类更改为AnnotationConfigApplicationContext类型

--- 0301-spring/spring_12_annotation_bean_manager/src/main/java/com/chaining/spring/annotation/AppForAnnotation.java
+++ 0301-spring/spring_12_annotation_bean_manager/src/main/java/com/chaining/spring/annotation/AppForAnnotation.java
@@ -8,8 +8,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppForAnnotation {
     public static void main(String[] args) {
         //AnnotationConfigApplicationContext加载Spring配置类初始化Spring容器
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
         BookDao bookDao = (BookDao) ctx.getBean("bookDao");
         System.out.println(bookDao);
+        ctx.close();
     }
 }
