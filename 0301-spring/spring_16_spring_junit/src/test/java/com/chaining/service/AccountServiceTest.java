55fa7ff7e7c2e28ea9410d83fa732e63c7d92292
chaining <ning.chai@foxmail.com>
2022-07-20 09:51:04 +0800

    feat: spring整合junit

---
+++ 0301-spring/spring_16_spring_junit/src/test/java/com/chaining/service/AccountServiceTest.java
@@ -0,0 +1,29 @@
+package com.chaining.service;
+
+import com.chaining.spring.mybatis.config.SpringConfig;
+import com.chaining.spring.mybatis.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p>
+ * description
+ * <p>
+ *
+ * @author <a href="mail to: ning.chai@foxmail.com" rel="nofollow">chaining</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class AccountServiceTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Test
+    public void testFindById(){
+        System.out.println(accountService.findById(1));
+    }
+}
